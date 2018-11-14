2b75399db1a8419480a262ab11500d7a218c048e
Adam Dorogi-Kaposi <adam.dorogi@gmail.com>
2018-11-14 22:39:24 +1000

    Initial Commit

---
+++ DarkMode/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DarkMode
+//
+//  Created by Adam Dorogi-Kaposi on 14/11/18.
+//  Copyright © 2018 Adam Dorogi-Kaposi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DarkMode/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DarkMode
+//
+//  Created by Adam Dorogi-Kaposi on 14/11/18.
+//  Copyright © 2018 Adam Dorogi-Kaposi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
