09460643c4c902184d476ab826cfce7565dc28cc
Adam Dorogi-Kaposi <adam.dorogi@gmail.com>
2018-11-18 11:51:09 +1000

    Initial commmit

--- DarkMode/AppDelegate.swift
+++ DarkMode/AppDelegate.swift
@@ -11,16 +11,45 @@ import Cocoa
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-
+    let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
+    let popover = NSPopover()
+    
+    var eventMonitor: Any?
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        // Create status bar item.
+        let image = NSImage(named: "owlOpen")
+        image?.size = NSSize(width: 20, height: 15)
+        if let button = statusItem.button {
+            button.image = image
+            button.action = #selector(togglePopover(_:))
+        }
+        
+        // Set up popover view controller.
+        popover.contentViewController = ViewController()
     }
-
-    func applicationWillTerminate(_ aNotification: Notification) {
-        // Insert code here to tear down your application
+    
+    @objc func togglePopover(_ sender: Any) {
+        if popover.isShown {
+            closePopover()
+        } else {
+            showPopover()
+        }
     }
-
-
+    
+    func showPopover() {
+        if let button = statusItem.button {
+            popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+            
+            // Add event monitor.
+            eventMonitor = NSEvent.addGlobalMonitorForEvents(matching: [.leftMouseDown, .rightMouseDown], handler: { _ in
+                self.closePopover()
+            })
+        }
+    }
+    
+    func closePopover() {
+        popover.close()
+        NSEvent.removeMonitor(eventMonitor!)
+    }
 }
-
--- DarkMode/ViewController.swift
+++ DarkMode/ViewController.swift
@@ -9,11 +9,43 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.view.frame = NSRect(x: 0, y: 0, width: 300, height: 100)
+        
+        let button = NSButton(title: "Toggle", target: self, action: #selector(toggleDarkMode(_:)))
+        self.view.addSubview(button)
+    }
+    
+    @objc func toggleDarkMode(_ sender: Any) {
+        //        let script = "tell application \"System Events\" to tell appearance preferences to set dark mode to not dark mode"
+        let script = """
+        tell application "System Events"
+            tell appearance preferences
+                set dark mode to not dark mode
+            end tell
+        end tell
+        """
+        
+        NSAppleScript(source: script)?.executeAndReturnError(nil)
+        
+        if isEnabled() {
+            let appDelegate = NSApplication.shared.delegate as! AppDelegate
+            appDelegate.statusItem.button!.image = NSImage(named: "owlOpen")
+        } else {
+            let appDelegate = NSApplication.shared.delegate as! AppDelegate
+            appDelegate.statusItem.button!.image = NSImage(named: "owlClosed")
+        }
+    }
+    
+    func isEnabled() -> Bool {
+        return UserDefaults.standard.string(forKey: "AppleInterfaceStyle") == "Dark"
+    }
+    
+    override func loadView() {
+        self.view = NSView()
     }
 
     override var representedObject: Any? {
@@ -21,7 +53,4 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
-
 }
-
