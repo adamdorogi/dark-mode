2be6bd3fd57e8126833aac04154bfece61086d1a
Adam Dorogi-Kaposi <adam.dorogi@gmail.com>
2019-06-10 17:46:44 +1000

    Various updates

--- DarkMode/AppDelegate.swift
+++ DarkMode/AppDelegate.swift
@@ -14,6 +14,7 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.squareLength)
     let popover = NSPopover()
     var eventMonitor: Any?
+    var viewController: ViewController!
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Create status bar item.
@@ -21,8 +22,11 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             button.action = #selector(togglePopover(_:))
         }
         
+        let storyboard = NSStoryboard(name: "Main", bundle: nil)
+        viewController = storyboard.instantiateController(withIdentifier: "test") as? ViewController
+        
         // Set up and show popover view controller.
-        popover.contentViewController = ViewController()
+        popover.contentViewController = viewController
         showPopover()
         
         // Add observer to detect change in dark mode.
@@ -32,10 +36,12 @@ class AppDelegate: NSObject, NSApplicationDelegate {
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
         if keyPath == "AppleInterfaceStyle", let button = statusItem.button {
             // Dark mode changed, update status item button image.
-            if change?[.newKey] is NSNull {
+            if change?[.newKey] is NSNull { // Dark mode off
                 button.image = NSImage(named: "Moon")
-            } else {
+                viewController.toggleButton.title = "Turn Dark Mode On"
+            } else { // Dark mode on
                 button.image = NSImage(named: "MoonDark")
+                viewController.toggleButton.title = "Turn Dark Mode Off"
             }
         }
     }
--- DarkMode/ViewController.swift
+++ DarkMode/ViewController.swift
@@ -9,71 +9,58 @@
 import Cocoa
 
 class ViewController: NSViewController {
+
+    @IBOutlet weak var gridView: NSGridView!
+    @IBOutlet weak var toggleButton: NSButton!
+    @IBOutlet weak var scheduleCheckBox: NSButton!
+    @IBOutlet weak var scheduleDropdown: NSPopUpButton!
+    @IBOutlet weak var smartAdjustCheckBox: NSButton!
+    
+    @IBOutlet weak var datePickerFrom: NSDatePicker!
+    @IBOutlet weak var brightnessSlider: NSSlider!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
-        // Initialise constants.
-        let horizontalPadding = CGFloat(30)
-        let verticalPadding = CGFloat(20)
-
-        let titleFontSize = CGFloat(28)
-        let headingFontSize = CGFloat(24)
-        let captionFontSize = CGFloat(10)
-
-        // Set up title label.
-        let titleLabel = NSTextField(labelWithString: "Night Owl")
-        titleLabel.font = .boldSystemFont(ofSize: titleFontSize)
-
-        // Set up toggle button.
-        let toggleButton = NSButton(title: "Toggle Dark Mode", target: self, action: #selector(toggleDarkMode(_:)))
-
-        // Set up status label.
-        let statusLabel = NSTextField(labelWithString: "Status")
-        statusLabel.font = .systemFont(ofSize: captionFontSize)
-
-        // Set up schedule checkbox.
-        let scheduleCheckbox = NSButton(checkboxWithTitle: "Schedule", target: self, action: #selector(toggleSchedule(_:)))
-        scheduleCheckbox.font = .boldSystemFont(ofSize: headingFontSize)
-
-        // Set up smart adjust checkbox.
-        let smartAdjustCheckbox = NSButton(checkboxWithTitle: "Smart Adjust", target: self, action: #selector(toggleSmartAdjust(_:)))
-        smartAdjustCheckbox.font = .boldSystemFont(ofSize: headingFontSize)
-
-        // Set up smart adjust label.
-        let smartAdjustLabel = NSTextField(labelWithString: "Turn on dark mode based on screen brightness.")
-        smartAdjustLabel.font = .systemFont(ofSize: captionFontSize)
         
-        // Set up grid view to store subviews.
-        let gridView = NSGridView(views: [[titleLabel], [toggleButton], [statusLabel], [scheduleCheckbox], [smartAdjustCheckbox], [smartAdjustLabel]])
-        gridView.cell(atColumnIndex: 0, rowIndex: 1).xPlacement = .center
-        gridView.cell(atColumnIndex: 0, rowIndex: 2).xPlacement = .center
-        gridView.row(at: 3).height = 21
-        gridView.row(at: 4).height = 21
-        gridView.frame.size = gridView.fittingSize
-        gridView.frame.origin = CGPoint(x: horizontalPadding, y: verticalPadding)
-        
-        self.view.frame.size = CGSize(width: gridView.frame.width + 2 * horizontalPadding, height: gridView.frame.height + 2 * verticalPadding)
-        self.view.addSubview(gridView)
+        toggleSchedule(scheduleCheckBox)
+        changeScheduleDropdown(scheduleDropdown)
+        toggleSmartAdjust(smartAdjustCheckBox)
+
+        DistributedNotificationCenter.default().addObserver(self, selector: #selector(smartAdjust), name: Notification.Name("com.apple.AmbientLightSensorHID.PreferencesChanged"), object: nil)
+        smartAdjust()
     }
     
-    @objc func toggleSchedule(_ sender: NSButton) {
-        if sender.state == .on {
-            print("Schedule on.")
-        } else {
-            print("Schedule off.")
+    @IBAction func changeBrightnessThreshold(_ sender: Any) {
+        smartAdjust()
+    }
+    
+    func getBrightness() -> Float {
+        var brightness: Float = 0.0
+        
+        var iterator: io_iterator_t = 0
+        
+        if IOServiceGetMatchingServices(kIOMasterPortDefault, IOServiceMatching("IODisplayConnect"), &iterator) == kIOReturnSuccess {
+
+            let service = IOIteratorNext(iterator)
+
+            IODisplayGetFloatParameter(service, 0, kIODisplayBrightnessKey as CFString, &brightness)
+            IOObjectRelease(service)
+        }
+        
+        return brightness * 100
+    }
+    
+    @objc func smartAdjust() {
+        if smartAdjustCheckBox.state == .on {
+            if getBrightness() <= brightnessSlider.floatValue {
+                darkModeOn()
+            } else {
+                darkModeOff()
+            }
         }
     }
     
-    @objc func toggleSmartAdjust(_ sender: NSButton) {
-        if sender.state == .on {
-            print("Smart adjust on.")
-        } else {
-            print("Smart adjust off.")
-        }
-    }
-    
-    @objc func toggleDarkMode(_ sender: NSButton) {
+    @IBAction func toggleDarkMode(_ sender: Any) {
         let toggleDarkModeScript = """
         tell application "System Events"
             tell appearance preferences
@@ -81,15 +68,45 @@ class ViewController: NSViewController {
             end tell
         end tell
         """
-        
+
         NSAppleScript(source: toggleDarkModeScript)?.executeAndReturnError(nil)
+    }
+    
+    func darkModeOff() {
+        if isEnabled() {
+            toggleDarkMode(self)
+        }
+    }
+    
+    func darkModeOn() {
+        if !isEnabled() {
+            toggleDarkMode(self)
+        }
+    }
+    
+    @IBAction func toggleSchedule(_ sender: NSButton) {
+        if sender.state == .on {
+            gridView.row(at: 4).isHidden = false
+        } else {
+            gridView.row(at: 4).isHidden = true
+        }
+        changeScheduleDropdown(scheduleDropdown)
+    }
+    
+    @IBAction func changeScheduleDropdown(_ sender: NSPopUpButton) {
+        if sender.title == "Custom" && scheduleCheckBox.state == .on {
+            gridView.row(at: 5).isHidden = false
+        } else {
+            gridView.row(at: 5).isHidden = true
+        }
+    }
+    
+    @IBAction func toggleSmartAdjust(_ sender: NSButton) {
+        gridView.row(at: 8).isHidden = (sender.state == .off)
+        smartAdjust()
     }
     
     func isEnabled() -> Bool {
         return UserDefaults.standard.string(forKey: "AppleInterfaceStyle") == "Dark"
-    }
-    
-    override func loadView() {
-        self.view = NSView()
     }
 }
